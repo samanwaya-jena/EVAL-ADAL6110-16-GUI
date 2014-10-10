eb12da1653ec0b318dfc2e8ccd853ac12b106741
JeanYvesDeschenes <Jean-Yves.Deschenes@aerostar.ca>
2014-10-10 08:40:45 -0400

    Restructured directories to introduce AWLCoreClasses.
    Introduced new AWLBareDemo project.
    Cleaned up unused files.

---
+++ src/AWLBareDemo/AWLBareDemo.cpp
@@ -0,0 +1,13 @@
+// AWLBareDemo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "awlsimpledemo.h"
+using namespace awl;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	AWLSimpleDemo app;
+	return app.exec();
+}
+
---
+++ src/AWLBareDemo/awlsimpledemo.h
@@ -0,0 +1,56 @@
+#ifndef AWLSIMPLEDEMO_H
+#define AWLSIMPLEDEMO_H
+/*
+	Copyright 2014 Aerostar R&D Canada Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+#include <boost/container/vector.hpp>
+
+
+#include "AWLSettings.h"
+#include "ReceiverCapture.h"
+
+namespace awl
+{
+
+
+
+class AWLSimpleDemo
+{
+//		public types and enums:
+public:
+public:
+	AWLSimpleDemo();
+	~AWLSimpleDemo();
+	int exec();
+
+
+protected:
+	void DisplayReceiverStatus();
+	void DisplayReceiverStatus(int receiverID);
+
+	bool DoOneLoopIteration();
+	bool GetLatestDetections(Detection::Vector &detectionData);
+	bool OutputDetections(const Detection::Vector &detectionData);
+
+private:
+	ReceiverCapture::List receiverCaptures;
+
+	/** \brief Our subscription identifier to access to lidar data. */
+	boost::container::vector<Publisher::SubscriberID> receiverCaptureSubscriberIDs;
+};
+} // namespace AWL          
+
+#endif // AWLSimpleDEMO_H
