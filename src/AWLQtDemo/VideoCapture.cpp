8d40c36234145321f53591bf45d0d196e5575c2b
JeanYvesDeschenes <Jean-Yves.Deschenes@aerostar.ca>
2015-12-31 01:56:10 -0500

    Removed direct calls to ximea drivers (which were now unused anyways). Eliminated the setting of XI_DATA_FORMAt to RGB32 (inefficient). Now uses default.

--- src/AWLQtDemo/VideoCapture.cpp
+++ src/AWLQtDemo/VideoCapture.cpp
@@ -75,33 +75,6 @@ public:
     XI_IMG    image;
 };
 
-// VideoCaptureDummy is just a descriptive class to allow us to recuperate the private cap member from the 
-// openCV CVCapture device.
-
-class VideoCaptureDummy
-{
-public:
-    CV_WRAP VideoCaptureDummy();
-    CV_WRAP VideoCaptureDummy(const std::string& filename);
-    CV_WRAP VideoCaptureDummy(int device);
-
-    virtual ~VideoCaptureDummy();
-    CV_WRAP virtual bool open(const std::string& filename);
-    CV_WRAP virtual bool open(int device);
-    CV_WRAP virtual bool isOpened() const;
-    CV_WRAP virtual void release();
-
-    CV_WRAP virtual bool grab();
-    CV_WRAP virtual bool retrieve(CV_OUT cv::Mat& image, int channel=0);
-    virtual VideoCapture& operator >> (CV_OUT cv::Mat& image);
-    CV_WRAP virtual bool read(CV_OUT cv::Mat& image);
-
-    CV_WRAP virtual bool set(int propId, double value);
-    CV_WRAP virtual double get(int propId);
-
-public:
-    cv::Ptr<CvCaptureCAM_XIMEA> cap;
-};
 
 VideoCapture::VideoCapture(int inCameraID, int argc, char** argv, boost::property_tree::ptree &propTree):
 ThreadedWorker(),
@@ -118,7 +91,9 @@ cameraID(inCameraID)
 
 	ReadConfigFromPropTree(propTree);
 
-//	ListCameras();
+#if 0
+	ListCameras();
+#endif
 	OpenCamera();
 }
 
@@ -142,11 +117,8 @@ void VideoCapture::CopyCurrentFrame(VideoCapture::FramePtr targetFrame, Publishe
 	{
 		//Instead of simply cloning, make sure the target comes out as a 3 channel BGR image.
 		targetFrame->create(currentFrame.rows, currentFrame.cols, CV_8UC3);
-#if 0
-		cv::cvtColor(currentFrame, *targetFrame, CV_BGRA2BGR);
-#else
 		*targetFrame = currentFrame.clone();
-#endif	
+
 		UnlockNews(inSubscriberID);
 	}
 };
@@ -284,7 +256,7 @@ void VideoCapture::ListCameras()
 
 
 
-	for (int i = 1100; i < 2000; i++) 
+	for (int i = 0; i < 2000; i++) 
 	{
 		DebugFilePrintf("Open %d", i);
 		cam.open(i);
@@ -323,100 +295,8 @@ bool VideoCapture::OpenCamera()
 		// If we are using the Ximea driver, set the downsampling for a 640x480 image
 		if (pref == CV_CAP_XIAPI)
 		{
-			HANDLE ximeaHandle = ((VideoCaptureDummy *)&cam)->cap->hmv; 
-			XI_RETURN xiRet = 0;
-
-			// Capture format for Ximea is RGB32.  Preferable over RGB24 for performance reasons, according to Ximea documentation.
-			cam.set(CV_CAP_PROP_XI_DATA_FORMAT, XI_RGB32 );
-			// Downsampling: Prefer XI_SKIPPING over XI_BINNING for performance reasons.
-//			xiRet = xiSetParamInt(ximeaHandle, XI_PRM_DOWNSAMPLING_TYPE, XI_SKIPPING);
-//			xiSetParamInt(ximeaHandle, XI_PRM_SHUTTER_TYPE, XI_SHUTTER_GLOBAL);
-
-			// Set color stuff
-#if 0
-			xiRet = xiSetParamInt(ximeaHandle, XI_PRM_AEAG, 1);  // Automatic exposure/gain enable
-			xiRet = xiSetParamInt(ximeaHandle, XI_PRM_AE_MAX_LIMIT, 80000); // Maximum exposure time, in microsec
-
-			xiRet = xiSetParamInt(ximeaHandle, XI_PRM_AG_MAX_LIMIT, 10.0); // Maximum limit of gain in AEAG procedure(dB).
-			xiRet = xiSetParamInt(ximeaHandle, XI_PRM_AEAG_LEVEL, 10);  //Average intensity of output signal AEAG should achieve(in %).
-			xiRet = xiSetParamFloat(ximeaHandle, XI_PRM_EXP_PRIORITY, 0.8);  // Priority Gain VS exposure. 0.0: Gain <------> 1.0 Exposure
-			xiRet = xiSetParamFloat(ximeaHandle, XI_PRM_GAMMAY, 0.5); // Luminosity gamma. Range: 0.3 (highest correction); 1 (no correction)
-			xiRet = xiSetParamFloat(ximeaHandle, XI_PRM_GAMMAC, 80.0/100.0); // Chromaticity gamma. Default: 0.8
-			xiRet = xiSetParamFloat(ximeaHandle, XI_PRM_SHARPNESS, 0.0);  //Sharpness Strength. The range is -4 (less sharp) to +4 (more sharp). Default: 0.0 (neutral)
-#endif
-#if 0
-			xiRet = xiSetParamInt(ximeaHandle, XI_PRM_AUTO_WB, 0);  // Auto white balance
-			xiRet = xiSetParamFloat(ximeaHandle, XI_PRM_WB_KR, 1.0);
-			xiRet = xiSetParamFloat(ximeaHandle, XI_PRM_WB_KG, 1.20);
-			xiRet = xiSetParamFloat(ximeaHandle, XI_PRM_WB_KB, 1.0);
-#else
-					xiRet = xiSetParamInt(ximeaHandle, XI_PRM_AUTO_WB, 1);  // Auto white balance
-#endif
-
-#if 0
-			float saturation  = (float)0.5;
-
-			float colorCorrectionMatrix[4][4];
-			colorCorrectionMatrix[0][0] = (float)(1.0+2*saturation);
-			colorCorrectionMatrix[0][1] = -saturation;
-			colorCorrectionMatrix[0][2] = -saturation;
-			colorCorrectionMatrix[0][3] = 0.0;
-
-			colorCorrectionMatrix[1][0] = -saturation;
-			colorCorrectionMatrix[1][1] = (float)(1.0+2*saturation);
-			colorCorrectionMatrix[1][2] = -saturation;
-			colorCorrectionMatrix[1][3] = 0.0;
-
-			colorCorrectionMatrix[2][0] = -saturation;
-			colorCorrectionMatrix[2][1] = -saturation;
-			colorCorrectionMatrix[2][2] = (float)(1.0+2*saturation);
-			colorCorrectionMatrix[2][3] = 0.0;
-
-			colorCorrectionMatrix[3][0] = 0.0;
-			colorCorrectionMatrix[3][1] = 0.0;
-			colorCorrectionMatrix[3][2] = 1.0;
-			colorCorrectionMatrix[3][3] = 0.0;
-
-
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_00, colorCorrectionMatrix[0][0]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_01, colorCorrectionMatrix[0][1]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_02, colorCorrectionMatrix[0][2]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_03, colorCorrectionMatrix[0][3]);
-
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_10, colorCorrectionMatrix[1][0]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_11, colorCorrectionMatrix[1][1]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_12, colorCorrectionMatrix[1][2]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_03, colorCorrectionMatrix[1][3]);
-
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_20, colorCorrectionMatrix[2][0]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_21, colorCorrectionMatrix[2][1]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_22, colorCorrectionMatrix[2][2]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_03, colorCorrectionMatrix[2][3]);
-
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_30, colorCorrectionMatrix[3][0]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_31, colorCorrectionMatrix[3][1]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_32, colorCorrectionMatrix[3][2]);
-			xiSetParamFloat(ximeaHandle, XI_PRM_CC_MATRIX_33, colorCorrectionMatrix[3][3]);
-#endif
-#if 0
-			// Disable color management for performance reasons
-			xiSetParamInt(ximeaHandle, XI_PRM_CMS, XI_CMS_DIS);
-#endif
-
 			// Set the amount of downsampling to get decent frame rate.
 			cam.set(CV_CAP_PROP_XI_DOWNSAMPLING, ximeaDefaultBinningMode);
-			// Always get the most recent frame.
-			xiSetParamInt(ximeaHandle, XI_PRM_RECENT_FRAME, XI_ON);
-
-		    // use minimum possible transport buffer size to optimize frame rate
-			int transportBufferSize;
-		    XI_RETURN stat = xiGetParamInt(ximeaHandle, XI_PRM_ACQ_TRANSPORT_BUFFER_SIZE XI_PRM_INFO_MIN, &transportBufferSize);
-			stat = xiSetParamInt(ximeaHandle, XI_PRM_ACQ_TRANSPORT_BUFFER_SIZE, transportBufferSize);
-
-			// set maximum number of queue
-			int numberOfFieldBuffers = 0;
-			stat = xiGetParamInt(ximeaHandle, XI_PRM_BUFFERS_QUEUE_SIZE XI_PRM_INFO_MAX, &numberOfFieldBuffers);
-			stat = xiSetParamInt(ximeaHandle, XI_PRM_BUFFERS_QUEUE_SIZE, numberOfFieldBuffers);
 		}
 
 		calibration.frameWidthInPixels = (int) cam.get(CV_CAP_PROP_FRAME_WIDTH);
