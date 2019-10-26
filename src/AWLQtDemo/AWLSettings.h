9c5275f13e36826059c389f4ba4571a8f849527b
JeanYvesDeschenes <jy.deschenes@phantomintelligence.com>
2019-10-26 12:21:46 -0400

    SensorSettings.h now handles general settings.
    SensorSettings.h handles general settings and allows derivation of applicatrion specific classes.
    AWLSettings.h and .cpp now handle application-specific settings.

--- src/AWLQtDemo/AWLSettings.h
+++ src/AWLQtDemo/AWLSettings.h
@@ -25,72 +25,13 @@
 
 #include "CoordinateSystem.h"
 #include "DetectionStruct.h"
+#include "SensorSettings.h"
 
-#define VelocityToKmH(velocity) (velocity * 3.6)
 
 namespace awl
 {
 
-
-	typedef struct ChannelConfig 
-	{
-		int channelIndex;
-		float fovWidth;
-		float fovHeight;
-
-		float maxRange;
-    float maxAscanRange;
-		uint8_t displayColorRed;
-		uint8_t displayColorGreen;
-		uint8_t displayColorBlue;
-	}
-	ChannelConfig;
-
-	typedef boost::container::vector<ChannelConfig> ChannelConfigVector;
-
-
-	typedef enum {
-		eVelocityUnitsMS = 0,
-		eVelocityUnitsKMH = 1
-	}
-	VelocityUnits;
-
-	typedef struct ReceiverSettings
-	{
-	// Channel configuration
-	ChannelConfigVector channelsConfig;
-
-	// Receiver
-	std::string sReceiverType;
-	std::string sReceiverRegisterSet;
-	std::string sReceiverChannelGeometry;
-
-
-	float displayedRangeMin;
-	float displayedRangeMax;
-	}
-	ReceiverSettings;
-
-	typedef boost::container::vector<ReceiverSettings> ReceiverSettingsVector;
-
-typedef struct CameraSettings
-	{
-	// Camera
-	std::string sCameraName;
-	std::string sCameraAPI;
-	bool cameraFlip;
-
-	CameraCalibration calibration;
-	float cameraFovWidthDegrees;
-	float cameraFovHeightDegrees;
-	float barrelK1;
-	float barrelK2;
-	}
-	CameraSettings;
-
-	typedef boost::container::vector<CameraSettings> CameraSettingsVector;
-
-class AWLSettings
+class AWLSettings: public SensorSettings
 {
 public:
 	static AWLSettings *InitSettings(const std::string sSettingsFileName = std::string(""));
@@ -99,44 +40,8 @@ public:
 	// Constructor
 	AWLSettings(const std::string sSettingsFileName = std::string(""));
 	bool ReadSettings();
-	boost::property_tree::ptree &GetPropTree() {return (propTree);};
-
-
-	// Get/Set methods
-
-	static void GetPosition(boost::property_tree::ptree &node, float &forward, float &left, float&up);
-
-	static void GetOrientation(boost::property_tree::ptree &node, float &pitch, float &yaw, float&roll);
-	static void Get2DPoint(boost::property_tree::ptree &node, float &x, float &y);
-
-	static void GetGeometry(boost::property_tree::ptree &geometryNode, float &forward, float &left, float &up, float &pitch, float &yaw, float &roll);
-	static void GetColor(boost::property_tree::ptree &colorNodeNode, uint8_t &red, uint8_t &green, uint8_t &blue);
-	static void GetAlertConditions(boost::property_tree::ptree &alertNode, AlertCondition &alert);
-
-	static void GetChannelGeometry(boost::property_tree::ptree &channelGeometryNode, ReceiverSettings *receiverPtr);
-	static void GetChannelGeometryArray(boost::property_tree::ptree &channelGeometryNode, ReceiverSettings *receiverPtr);
-
-
-	static void PutPosition(boost::property_tree::ptree &node, float forward, float left, float up);
-	static void PutOrientation(boost::property_tree::ptree &node, float pitch, float yaw, float roll);
-	static void Put2DPoint(boost::property_tree::ptree &node, float x, float y);
-	static void PutGeometry(boost::property_tree::ptree &geometryNode, float forward, float left, float up, float pitch, float yaw, float roll);
-	static void PutColor(boost::property_tree::ptree &colorNode, uint8_t red, uint8_t green, uint8_t blue);
-
-	static void PutChannelGeometry(boost::property_tree::ptree &channelGeometryNode, ReceiverSettings *receiverPtr);
-	/** \brief Stores the current receiver calibration settings
-		* \return true if storage processe dwithout error. False in case of a storage error.
-      */
-	//bool AWLSettings::StoreReceiverCalibration();
-	//Linux
-	bool StoreReceiverCalibration();
-
+	
 public:
-	// Receiver configuration
-	ReceiverSettingsVector receiverSettings;
-
-	// Camera configuration
-	CameraSettingsVector cameraSettings;
 
 	// Layout
 	bool bDisplaySettingsWindow;
@@ -189,21 +94,6 @@ public:
 	bool bDisplayVideoCrosshair;
 	bool bDisplayVideoTime;
 
-
-	// Dynamic testing
-	float brakingDeceleration;
-	float travelSpeed;
-
-	// Debug
-	bool bWriteDebugFile;
-	bool bWriteLogFile;
-
-
-protected:
-	std::string sFileName;
-	static AWLSettings *globalSettings;
-	// Property tree contains all the information from the configuration file
-	boost::property_tree::ptree propTree;
 };
 
 } // namespace AWL          
